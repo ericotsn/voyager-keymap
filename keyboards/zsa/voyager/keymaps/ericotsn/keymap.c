79678b5f90c5013ced86403c2bcef5205094a239
Eric Ottosson <contact@ericotsn.com>
2026-08-31 21:35:25 +0200

    Remove gaming layer

--- keyboards/zsa/voyager/keymaps/ericotsn/keymap.c
+++ keyboards/zsa/voyager/keymaps/ericotsn/keymap.c
@@ -5,7 +5,6 @@
 
 enum layer_names {
     _BASE,
-    _GAME,
     _SYM,
     _EXT,
     _NUM,
@@ -62,18 +61,11 @@ enum keycode_aliases {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, TG(_GAME),
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
         _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
         _______, HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                         KC_H,    HM_J,    HM_K,    HM_L,    HM_QUOT, _______,
         _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UNDS, _______,
                                                      LA_EXT,  KC_SPC,     OS_LSFT, LA_SYM
-    ),
-    [_GAME] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         _______, _______, _______, _______, _______, _______,
-        KC_T,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                         _______, _______, _______, _______, _______, _______,
-        KC_G,    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                         _______, _______, _______, _______, _______, _______,
-        KC_B,    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                         _______, _______, _______, _______, _______, _______,
-                                                     KC_SPC, _______,     _______, _______
     ),
     [_SYM] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
