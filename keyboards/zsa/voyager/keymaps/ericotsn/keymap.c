60624451f0bb94585fe689a920688fac4e36abd2
Eric Ottosson <contact@ericotsn.com>
2026-09-24 19:40:43 +0200

    New and improved Voyager keymap

--- keyboards/zsa/voyager/keymaps/ericotsn/keymap.c
+++ keyboards/zsa/voyager/keymaps/ericotsn/keymap.c
@@ -28,7 +28,7 @@ enum keycode_aliases {
     HRM_J   = RCTL_T(KC_J),
     HRM_K   = RSFT_T(KC_K),
     HRM_L   = RALT_T(KC_L),
-    HRM_QUO = RGUI_T(KC_QUOT),
+    HRM_REP = RGUI_T(QK_REP),
 };
 
 // clang-format off
@@ -36,23 +36,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
         CW_TOGG, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        QK_REP,  HRM_A,   HRM_S,   HRM_D,   HRM_F,   KC_G,                         KC_H,    HRM_J,   HRM_K,   HRM_L,   HRM_QUO, KC_MINS,
-        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+        KC_UNDS, HRM_A,   HRM_S,   HRM_D,   HRM_F,   KC_G,                         KC_H,    HRM_J,   HRM_K,   HRM_L,   HRM_REP, KC_MINS,
+        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, _______,
                                                      MO(EXT), KC_SPC,     OS_LSFT, MO(SYM)
     ),
     [SYM] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
         _______, KC_GRV,  KC_LABK, KC_RABK, KC_MINS, KC_PIPE,                      KC_CIRC, KC_LCBR, KC_RCBR, KC_DLR,  _______, _______,
         _______, KC_EXLM, KC_ASTR, KC_SLSH, KC_EQL,  KC_AMPR,                      KC_HASH, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO, _______,
-        _______, KC_TILD, KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC,                      KC_AT,   KC_COLN, KC_COMM, KC_DOT,  _______, _______,
-                                                     KC_UNDS, _______,    _______, _______
+        _______, KC_TILD, KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC,                      KC_AT,   KC_COLN, KC_COMM, KC_DOT,  KC_QUOT, _______,
+                                                     _______, _______,    _______, _______
     ),
     [EXT] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_MSTP, KC_VOLU,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_F12,
-        _______, OS_LGUI, OS_LALT, OS_LSFT, OS_LCTL, KC_VOLD,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, _______,
-        _______, OS_UNDO, OS_CUT,  OS_COPY, OS_PSTE, KC_MUTE,                      _______, KC_TAB,  PRV_TAB, NXT_TAB, KC_ENT,  _______,
-                                                     _______, _______,    _______, KC_ESC
+        _______, _______, _______, PRV_TAB, NXT_TAB, _______,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_F12,
+        _______, OS_LGUI, OS_LALT, OS_LSFT, OS_LCTL, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, _______,
+        _______, OS_UNDO, OS_CUT,  OS_COPY, OS_PSTE, _______,                      _______, KC_ESC,  KC_TAB,  _______, KC_ENT,  _______,
+                                                     _______, _______,    _______, _______
     ),
 };
 // clang-format on
@@ -73,6 +73,15 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
 // clang-format on
 
 ///////////////////////////////////////////////////////////////////////////////
+// Key Overrides (https://docs.qmk.fm/features/key_overrides)
+///////////////////////////////////////////////////////////////////////////////
+
+const key_override_t dot_key_override  = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_QUES);  // Shift . is ?
+const key_override_t comm_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_EXLM); // Shift , is !
+
+const key_override_t *key_overrides[] = {&comm_key_override, &dot_key_override};
+
+///////////////////////////////////////////////////////////////////////////////
 // Custom Quantum Functions (https://docs.qmk.fm/custom_quantum_functions)
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -86,32 +95,47 @@ static void tap_primary_mod(uint16_t keycode) {
     tap_code16(detected_host_os() == OS_MACOS ? G(keycode) : C(keycode));
 }
 
-static bool process_os_shortcuts(uint16_t keycode, keyrecord_t *record) {
-    if (!record->event.pressed) {
-        return true;
-    }
-
-    switch (keycode) {
-        case OS_CUT:
-            tap_primary_mod(KC_X);
-            return false;
-        case OS_COPY:
-            tap_primary_mod(KC_C);
-            return false;
-        case OS_PSTE:
-            tap_primary_mod(KC_V);
-            return false;
-        case OS_UNDO:
-            tap_primary_mod(KC_Z);
-            return false;
+bool remember_last_key_user(uint16_t keycode, keyrecord_t *record, uint8_t *remembered_mods) {
+    if (keycode == HRM_REP) {
+        return false;
     }
 
     return true;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_os_shortcuts(keycode, record)) {
-        return false;
+    switch (keycode) {
+        case HRM_REP:
+            if (record->tap.count) {
+                repeat_key_invoke(&record->event);
+                return false;
+            }
+            break;
+
+        case OS_CUT:
+            if (record->event.pressed) {
+                tap_primary_mod(KC_X);
+                return false;
+            }
+            break;
+        case OS_COPY:
+            if (record->event.pressed) {
+                tap_primary_mod(KC_C);
+                return false;
+            }
+            break;
+        case OS_PSTE:
+            if (record->event.pressed) {
+                tap_primary_mod(KC_V);
+                return false;
+            }
+            break;
+        case OS_UNDO:
+            if (record->event.pressed) {
+                tap_primary_mod(KC_Z);
+                return false;
+            }
+            break;
     }
 
     return true;
--- keyboards/zsa/voyager/keymaps/ericotsn/rules.mk
+++ keyboards/zsa/voyager/keymaps/ericotsn/rules.mk
@@ -1,5 +1,6 @@
 # Copyright 2026 Eric Ottosson (@ericotsn) <contact@ericotsn.com>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+KEY_OVERRIDE_ENABLE = yes
 OS_DETECTION_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
